46fb2cfb6d88abe11771a6021eec7be240f5e0f5
luisjar <luismigueljaramillo@hotmail.com>
2015-01-27 14:00:00 -0500

    Added full functionaliy of Google maps, working on styling

--- Sneaky/app/assets/javascripts/application.js
+++ Sneaky/app/assets/javascripts/application.js
@@ -40,20 +40,92 @@ var App = {
 			function getLocation() {
     	if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
+        
    	  } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     	}
 		}
+
 		function showPosition(position) {
-			var lat = position.coords.latitude;
-			var long = position.coords.longitude;
+			lat = position.coords.latitude;
+			long = position.coords.longitude;
     	x.innerHTML = "Latitude: " + lat + 
     	"<br>Longitude: " + long; 
 
     	$('#upload_latitude').attr('value', lat)
     	$('#upload_longitude').attr('value', long)
+
+    	googleMaps();
 		}
 		getLocation()
+
+
+		function googleMaps() {
+    var map;
+    var elevator;
+    var myLatlng = new google.maps.LatLng($('#upload_latitude').attr('value'), $('#upload_longitude').attr('value'));
+    var myOptions = {
+        zoom: 14,
+        center: myLatlng,
+        mapTypeId: 'roadmap'
+    };
+    map = new google.maps.Map($('#map_canvas')[0], myOptions);
+
+    var marker = new google.maps.Marker({
+    	position: myLatlng,
+    	title: "Your current location",
+    	icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+		});
+
+    var currentCircle;
+
+		var currentCircle = new google.maps.Circle({
+			fillColor: '#7D8EE2',
+			fillOpacity: 0.35,
+			map:map,
+			center: myLatlng,
+			radius: 1600,
+			strokeWeight: 0
+		});
+
+		marker.setMap(map);
+
+    var addresses = [];
+    var picturesForMarkers = [];
+		var divPictureLatitude = $('.pictures');
+		$.each(divPictureLatitude,function(i,val){
+			addresses.push(val.children[1].value + ", " + val.children[2].value);
+			picturesForMarkers.push(val.children[0].src);
+
+		});
+
+		var infowindow = new google.maps.InfoWindow();
+		var i;
+
+    for (var x = 0; x < addresses.length; x++) {
+        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+            var marker = new google.maps.Marker({
+                position: latlng,
+                map: map
+            });
+
+
+for (var x = 0; x < picturesForMarkers.length; x++) {
+        google.maps.event.addListener(marker, 'click', (function(marker, x) {
+          return function() {
+             var content='<img src="'+ picturesForMarkers[x] +'" style="width:300px;">';
+             console.log(content);
+             infowindow.setContent(content);
+             infowindow.open(map, marker);
+            	}	
+        		})(marker, x));
+    
+}
+        });
+    	}
+		};
 	}
 
 
